ff3b9669690f4b7039fa54ad3e44ac51901273a9
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-11-13 09:31:02 +0100

    Modification de la méthode setAddUserEltCallback: ajout d'un intervale de génération d'age

--- src/users-ui.js
+++ src/users-ui.js
@@ -9,6 +9,6 @@ export function createHtmlUser(age) {
 
 export function setAddUserEltCallback(addUserElt, usersElt){
    addUserElt.addEventListener("click",()=> {
-       usersElt.appendChild(createHtmlUser(Math.round(Math.random()*100)))
+       usersElt.appendChild(createHtmlUser(Math.round(Math.random()*(32-12)+12)))
    });
 }
