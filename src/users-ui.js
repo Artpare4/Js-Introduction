ba076def9d8ea0a3e7df38f15aa466b5b7d96895
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-11-13 09:26:21 +0100

    Ajout de la gestion du clic de la souris sur le bouton d'ajout

--- src/users-ui.js
+++ src/users-ui.js
@@ -1,6 +1,14 @@
+import {add} from "./introduction";
+
 export function createHtmlUser(age) {
     const liElt = document.createElement("li");
     liElt.className = "user";
     liElt.innerHTML = age;
     return liElt;
+}
+
+export function setAddUserEltCallback(addUserElt, usersElt){
+   addUserElt.addEventListener("click",()=> {
+       usersElt.appendChild(createHtmlUser(17))
+   });
 }
