16e3e5aa6f820ca82bd44acac5288236466d17a4
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-11-26 23:42:55 +0100

    Ajout de la réponse à la partie 25

--- src/users-ui.js
+++ src/users-ui.js
@@ -1,10 +1,9 @@
-import {add, ageAverage} from "./introduction";
+import {add, adultFilter, ageAverage} from "./introduction";
 
 export function createHtmlUser(age) {
-    const liElt = document.createElement("li");
-    liElt.className = "user";
-    liElt.innerHTML =
-        `<li>
+  const liElt = document.createElement("li");
+  liElt.className = "user";
+  liElt.innerHTML = `<li>
             <form class='user'> 
                 <label>
                     <input type="text" class="input user__name" value="Name">
@@ -20,34 +19,44 @@ export function createHtmlUser(age) {
                 </button>
             </form>
         </li>`;
-    return liElt;
+  return liElt;
 }
 
-export function setAddUserEltCallback(addUserElt, usersElt){
-   addUserElt.addEventListener("click",()=> {
-       usersElt.appendChild(createHtmlUser(Math.round(Math.random()*(32-12)+12)))
-   });
-}
-export function extractUser(userElt){
-    let user={
-        name:userElt.querySelector(".user__name").value,
-        age:parseInt(userElt.querySelector(".user__age").value)
-
-    };
-    return user;
+export function extractUser(userElt) {
+  const user = {
+    name: userElt.querySelector(".user__name").value,
+    age: parseInt(userElt.querySelector(".user__age").value),
+  };
+  return user;
 }
 
-export function extractUsers(usersElt){
-    var liste=usersElt.querySelectorAll("li.user");
-    liste=Array.from(liste,extractUser);
-    return liste;
+export function extractUsers(usersElt) {
+  let liste = usersElt.querySelectorAll("li.user");
+  liste = Array.from(liste, extractUser);
+  return liste;
 }
 
-export function extractUserType(infoElt){
-    return infoElt.querySelector(":checked").value;
+export function extractUserType(infoElt) {
+  return infoElt.querySelector(":checked").value;
 }
 
-export function  updateAgeAverage(usersElt,infoElt){
-    var users=extractUsers(usersElt);
-    var avg=ageAverage(users);
+export function updateAgeAverage(usersElt, infoElt) {
+  const users = extractUsers(usersElt);
+  const type = extractUserType(infoElt);
+  const avg = ageAverage(users, type);
+  const avgStr = avg ? parseFloat(avg).toFixed(2) : "";
+  const averageElt = infoElt.querySelector(".info__age-average");
+
+  averageElt.innerText = avgStr;
+}
+export function setAddUserEltCallback(addUserElt, usersElt, infoElt = "") {
+  addUserElt.addEventListener("click", () => {
+    usersElt.appendChild(
+      createHtmlUser(Math.round(Math.random() * (32 - 12) + 12)),
+    );
+    if(infoElt!==""){
+      updateAgeAverage(usersElt, infoElt.querySelector("article.info").value);
+    }
+
+  });
 }
